4915f552afce59d6f85344d8aa24e4a4928eed3b
Julie Pagano <julie.pagano@gmail.com>
2012-02-11 00:06:33 -0500

    Only run blog ajax when blog container exists.

--- source/javascripts/wp_blog.js
+++ source/javascripts/wp_blog.js
@@ -3,24 +3,26 @@
 var MYBLOG_LIMIT = 3;
 var MYWRAPPER_ID = '#recent_blog_posts';
 
-$.ajax({
-  url: "http://juliepagano.com/blog/api/get_recent_posts?count=" + MYBLOG_LIMIT,
-  dataType: 'jsonp',
-  success: function(data, textStatus, jqXHR) {
-    console.log(data);
-    var container = $(MYWRAPPER_ID);
-    var posts = data.posts;
-    container.html("");
-    for(var i=0; i < posts.length; i++) {
-      var post = posts[i];
-      var post_html = "<a href='" + post.url + "'>";
-      post_html += "<h4>" + post.title + "</h4>";
-      post_html += "</a>";
-      post_html += "<p>" + post.excerpt + "</p>";
-      container.append(post_html);
+if($(MYWRAPPER_ID).length) {
+  $.ajax({
+    url: "http://juliepagano.com/blog/api/get_recent_posts?count=" + MYBLOG_LIMIT,
+    dataType: 'jsonp',
+    success: function(data, textStatus, jqXHR) {
+      console.log(data);
+      var container = $(MYWRAPPER_ID);
+      var posts = data.posts;
+      container.html("");
+      for(var i=0; i < posts.length; i++) {
+        var post = posts[i];
+        var post_html = "<a href='" + post.url + "'>";
+        post_html += "<h4>" + post.title + "</h4>";
+        post_html += "</a>";
+        post_html += "<p>" + post.excerpt + "</p>";
+        container.append(post_html);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // FIXME - add error handling
     }
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-    // FIXME - add error handling
-  }
-});
+  });
+}
