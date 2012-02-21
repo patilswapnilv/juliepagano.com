1e254602fcfc175ff393f25ac0a22a862fe7b60f
Julie Pagano <julie.pagano@gmail.com>
2012-02-20 23:01:53 -0500

    Added handlebars functionality for loading blog posts.

--- source/javascripts/wp_blog.js
+++ source/javascripts/wp_blog.js
@@ -1,4 +1,7 @@
 // FIXME to be cleaned up - potentially converted to coffeescript
+Handlebars.registerHelper("blogTimestamp", function(post) {
+  return jQuery.timeago(new Date(this.date));
+});
 
 var MYBLOG_LIMIT = 3;
 var MYWRAPPER_ID = '#recent_blog_posts';
@@ -8,19 +11,7 @@ if($(MYWRAPPER_ID).length) {
     url: "http://juliepagano.com/blog/api/get_recent_posts?count=" + MYBLOG_LIMIT,
     dataType: 'jsonp',
     success: function(data, textStatus, jqXHR) {
-      var container = $(MYWRAPPER_ID);
-      var posts = data.posts;
-      container.html("");
-      for(var i=0; i < posts.length; i++) {
-        var post = posts[i];
-        var post_html = "<div class='post'>"
-        post_html += "<a href='" + post.url + "'>";
-        post_html += "<h4>" + post.title + "</h4>";
-        post_html += "</a>";
-        post_html += "<p>" + post.excerpt + "</p>";
-        post_html += "</div>"
-        container.append(post_html);
-      }
+      $(MYWRAPPER_ID).handlebars($('#blog-posts-template'), data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       // FIXME - add error handling
