a461362016966621db8778ca3c5e27878ebc86a7
Julie Pagano <julie.pagano@gmail.com>
2012-05-06 22:53:51 -0400

    Modify url and post count for wp blog

--- source/javascripts/wp_blog.js
+++ source/javascripts/wp_blog.js
@@ -3,12 +3,12 @@ Handlebars.registerHelper("blogTimestamp", function(post) {
   return jQuery.timeago(new Date(this.date));
 });
 
-var MYBLOG_LIMIT = 3;
+var MYBLOG_LIMIT = 5;
 var MYWRAPPER_ID = '#recent_blog_posts';
 
 if($(MYWRAPPER_ID).length) {
   $.ajax({
-    url: "http://juliepagano.com/blog/api/get_recent_posts?count=" + MYBLOG_LIMIT,
+    url: "http://juliepagano.com/blog/api/get_recent_posts/?count=" + MYBLOG_LIMIT,
     dataType: 'jsonp',
     success: function(data, textStatus, jqXHR) {
       $(MYWRAPPER_ID).handlebars($('#blog-posts-template'), data);
