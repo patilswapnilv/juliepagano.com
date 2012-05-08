9c3cf5e89f28c2e4b290bf3260f9322e246f9c90
Julie Pagano <julie.pagano@gmail.com>
2012-05-07 22:15:40 -0400

    Upgrade to middleman 3.0.0.beta.2

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -2,8 +2,13 @@ module CustomHelpers
   def nav_link(label, path, class_name = nil)
     class_name ||= path.delete("/")
 
+    current_path = request.path.match(/\/.*?\//) || "index"
+    current_path = current_path.to_s.gsub("/", "")
+
     link_class = [class_name]
-    link_class << "active" if request.path.split(".").first == path
+    link_class << "active" if current_path == path
+
+    path = "/" if path == "index"
     link_to label, path, :class => link_class.join(" ")
   end
 end
